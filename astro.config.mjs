794320ccf7b629963fff1f9d9bcd364e81c3f81e
naft-nite <sapkovich.vladislav@gmail.com>
2023-03-28 11:33:13 +0300

    localization WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import astroI18next from "astro-i18next";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [preact(), astroI18next()],
   site: 'https://Nominy.github.io',
 });
