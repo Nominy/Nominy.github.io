513e2f7e6a900c42e00abe7ef58f9b3c3186691f
naft-nite <39337311+Nominy@users.noreply.github.com>
2025-06-05 00:44:06 +0300

    fix i18n configuration and translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import astroI18next from "astro-i18next";
+import astroI18nextConfig from "./astro-i18next.config.cjs";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), astroI18next()],
+  integrations: [preact(), astroI18next({ config: astroI18nextConfig })],
   site: 'https://www.crystalgate.life',
 });
